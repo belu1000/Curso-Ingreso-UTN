4efe3030cb4b6dc33132cbc92ab950229baaba9c
pedro-geraghty-sw <pedro.geraghty@southworks.com>
2023-02-04 20:59:38 -0300

    Revision y correccion de TP 1

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -3,62 +3,71 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
-{  let precioUno;
-   let precioDos;
-   let precioTres;
-   let resultado;
-   
+function Sumar() {
+  let precioUno;
+  let precioDos;
+  let precioTres;
+  let resultado;
 
 
-   precioUno = document.getElementById("txtIdPrecioUno").value;
-   precioDos = document.getElementById("txtIdPrecioDos").value;
-   precioTres = document.getElementById("txtIdPrecioTres").value;
 
-   precioUno = parseInt(precioUno);
-   precioDos = parseInt(precioDos);
-   precioTres = parseInt(precioTres);
+  precioUno = document.getElementById("txtIdPrecioUno").value;
+  precioDos = document.getElementById("txtIdPrecioDos").value;
+  precioTres = document.getElementById("txtIdPrecioTres").value;
 
-   resultado = precioUno + precioDos + precioTres;
+  precioUno = parseInt(precioUno);
+  precioDos = parseInt(precioDos);
+  precioTres = parseInt(precioTres);
 
-   alert(" EL RESULTADO DE LA SUMA ES " + resultado);
+  resultado = precioUno + precioDos + precioTres;
 
-	
+  alert(" EL RESULTADO DE LA SUMA ES " + resultado);
+
+
 }
-function Promedio () 
-{
-      let precioUno;
-        let precioDos;
-        let precioTres;
-        let resultado;
-        let suma;
-     
-     
-        precioUno = document.getElementById("txtIdPrecioUno").value;
-        precioDos = document.getElementById("txtIdPrecioDos").value;
-        preciotres = document.getElementById("txtIdPrecioTres").value;
+function Promedio() {
+  let precioUno;
+  let precioDos;
+  let precioTres;
+  let suma;
+  let resultadoDeSuma;
 
-        suma = precioUno + precioDos + precioTres;
+  precioUno = document.getElementById("txtIdPrecioUno").value;
+  precioUno = parseInt(precioUno);
+  precioDos = document.getElementById("txtIdPrecioDos").value;
+  precioDos = parseInt(precioDos);
+  precioTres = document.getElementById("txtIdPrecioTres").value;
+  precioTres = parseInt(precioTres);
 
-        alert(suma / 3);
 
-function PrecioFinal () 
-{
-        
-      let precioUno;
-      let precioDos;
-      let precioTres;
-      let resultado;
-      let 
-   
-   
-      precioUno = document.getElementById("txtIdPrecioUno").value;
-      precioDos = document.getElementById("txtIdPrecioDos").value;
-      precioTres = document.getElementById("txtIdPrecioTres").value;
-     
-        suma = precioUno + precioDos + precioTres
-        prompt()
+  suma = precioUno + precioDos + precioTres;
+  resultadoDeSuma = suma / 3;
 
-        alert()
-	
+  alert( " El resultado del Promedio es " +  resultadoDeSuma);
 }
+
+
+
+  function PrecioFinal() {
+
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let suma;
+    const IVA = 21;
+    let resultadoIva;
+    let resultadoFinal;
+
+    precioUno = document.getElementById("txtIdPrecioUno").value;
+    precioUno = parseInt(precioUno);
+    precioDos = document.getElementById("txtIdPrecioDos").value;
+    precioDos = parseInt(precioDos);
+    precioTres = document.getElementById("txtIdPrecioTres").value;
+    precioTres = parseInt(precioTres);
+    suma = precioUno + precioDos + precioTres;
+    resultadoIva = (suma * IVA ) / 100;
+    resultadoFinal = resultadoIva;
+    
+    alert("El precio final es " + resultadoFinal );
+
+  }
