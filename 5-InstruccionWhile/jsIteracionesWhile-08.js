6f9cdc16c4c376f4721d2686540acc0ef6972bfd
pedro-geraghty-sw <pedro.geraghty@southworks.com>
2023-02-16 09:49:15 -0300

    While ejercicio 8 9 empezados

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,10 +3,10 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	var contador;
-	var respuesta;
-	var sumaPositivos;
-	var multiplicacionNegativos;
+	let contador;
+	let respuesta;
+	let sumaPositivos;
+	let multiplicacionNegativos;
 	contador=0;
 	sumaPositivos=0;
 	multiplicacionNegativos=1;
--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,19 +3,50 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
+	let banderaDelPrimero;
+	let numeroIngresado;
+	let numeroMaximo = 10
+	let numeroMinimo = 0
+	let respuesta;
+	let contador;
 	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
+	banderaDelPrimero=0;
+	respuesta='s';
+
+	contador=0;
+
+	while(respuesta=="s")
 	{
-		
+		numeroIngresado = prompt("Ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
 		respuesta=prompt("desea continuar?");
+		if (banderaDelPrimero == 0) 
+		{
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+			banderaDelPrimero = 1
+
+			contador = contador + 1;
+			if (contador == 1)
+			{
+				numeroMaximo = numeroIngresado;
+				numeroMinimo = numeroIngresado;
+			}
+			
+			else
+			 {
+				if (numeroIngresado > numeroMaximo){
+					numeroMaximo = numeroIngresado;
+				}
+				if (numeroIngresado < numeroMinimo){
+					numeroMinimo = numeroIngresado;
+				}
+			}
+		}
+		
+		respuesta = prompt("Desea continuar");
+
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	document.getElementById("txtIdMaximo").value=numeroMaximo;
+	document.getElementById("txtIdMinimmo").value=numeroMinimo;
 }//FIN DE LA FUNCIÓN
